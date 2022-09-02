a814b6b6a1e4a5eb08de880e38827d1234c55253
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-09-02 23:49:40 +0530

    manue add in api

--- seript.js
+++ seript.js
@@ -1,1 +1,22 @@
-console.log('iam reygds')
+// main api lode 
+const lodeApi=()=>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(valu => lodeManu(valu.data.news_category))
+}
+const lodeManu=manus=>{
+    // console.log(manus.data.news_category)
+    const manuDive =document.getElementById('manu-div');
+    for(const manu of manus){
+        console.log(manu.category_name)
+        const li =document.createElement('li')
+        li.style.listStyle=('none')
+        li.classList.add('p-4')
+
+        li.innerHTML=`
+        <a href="" class='text-decoration-none text-danger'>${manu.category_name}</a>
+        `
+        manuDive.appendChild(li)
+    }
+}
+lodeApi()
